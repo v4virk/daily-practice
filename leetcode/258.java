94dd63f06a7c9bdb78cc2aa68fcbad450ba1f946
HARVIJAY SINGH <harvijay.virk123@gmail.com>
2025-08-28 01:51:26 +0530

    day 120

---
+++ leetcode/258.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int addDigits(int num) {
+        while (num >= 10) {  // Keep reducing until single digit
+            int sum = 0;
+            while (num > 0) {  // Sum all digits
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum; // Replace num with sum for next iteration
+        }
+        return num; // Single digit result
+    }
+}
