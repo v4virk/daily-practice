005df16d43e7ab418c9a1a2b9771c0240c5b0608
HARVIJAY SINGH <harvijay.virk123@gmail.com>
2025-08-31 05:35:25 +0530

    3 iterations

--- programs/arrays/1.java
+++ programs/arrays/1.java
@@ -41,3 +41,21 @@ class Solution {
         
     }
 }
+
+
+//using hashmap
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        
+        return new int[] {-1, -1}; // if no solution
+    }
+}
