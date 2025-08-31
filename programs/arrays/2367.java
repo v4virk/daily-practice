f767f575423c05751f022aa498694e47a8e654bc
HARVIJAY SINGH <harvijay.virk123@gmail.com>
2025-08-31 11:07:19 +0530

    day 21

---
+++ programs/arrays/2367.java
@@ -0,0 +1,24 @@
+// triplets with common difference
+
+class Solution {
+    public int arithmeticTriplets(int[] nums, int diff) {
+        int count = 0;
+
+        if(nums.length < 3){
+            return 0;
+        }
+
+        for(int i = 0 ; i< nums.length-2; i++){
+            for(int j = i+1; j< nums.length-1; j++){
+                for(int k = j+1; k < nums.length; k++){
+                    if(nums[j] - nums[i] == diff && nums[k] - nums[j] == diff){
+                        count++;
+
+                    }
+                }
+            }
+        }
+        return count;
+        
+    }
+}
