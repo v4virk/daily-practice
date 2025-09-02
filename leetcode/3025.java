0fc61d9f3ce61489d16585f42a950f6038ba831f
HARVIJAY SINGH <harvijay.virk123@gmail.com>
2025-09-02 23:08:51 +0530

    day 23

---
+++ leetcode/3025.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int numberOfPairs(int[][] points) {
+        int n = points.length;
+
+        // Sort: x ascending, if x same then sort as y descending
+        Arrays.sort(points, (point1, point2) -> {
+            if (point1[0] == point2[0]) {
+                return point2[1] - point1[1];
+            }
+            return point1[0] - point2[0];
+        });
+
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            int x1 = points[i][0];
+            int y1 = points[i][1];   // upper left
+
+            int bestY = Integer.MIN_VALUE;
+
+            for (int j = i + 1; j < n; j++) {
+                int x2 = points[j][0];
+                int y2 = points[j][1];   // lower right
+
+                // Condition: (x2, y2) must be above (x1, y1)
+                if (y2 > y1) { //not lower right
+                    continue;
+                }
+
+                if (y2 > bestY) {
+                    result++;
+                    bestY = y2;
+                }
+            }
+        }
+
+        return result;
+    }
+}
