21d814c77abf318e0d9d6fdc80d058e6fb05e92a
HARVIJAY SINGH <harvijay.virk123@gmail.com>
2025-09-03 09:01:34 +0530

    day 24

--- leetcode/3025.java
+++ leetcode/3025.java
@@ -1,41 +1,47 @@
-import java.util.*;
+// iteration 1 brute force
 
 class Solution {
     public int numberOfPairs(int[][] points) {
-        int n = points.length;
+        int count = 0;
 
-        // Sort: x ascending, if x same then sort as y descending
-        Arrays.sort(points, (point1, point2) -> {
-            if (point1[0] == point2[0]) {
-                return point2[1] - point1[1];
-            }
-            return point1[0] - point2[0];
-        });
+        // upper valid point     
+        for(int i = 0; i<points.length; i++){
 
-        int result = 0;
-
-        for (int i = 0; i < n; i++) {
             int x1 = points[i][0];
-            int y1 = points[i][1];   // upper left
+            int y1 = points[i][1];
 
-            int bestY = Integer.MIN_VALUE;
-
-            for (int j = i + 1; j < n; j++) {
-                int x2 = points[j][0];
-                int y2 = points[j][1];   // lower right
-
-                // Condition: (x2, y2) must be above (x1, y1)
-                if (y2 > y1) { //not lower right
+            //lower valid point
+            for(int j = 0; j<points.length; j++){
+                if(j == i){
                     continue;
                 }
 
-                if (y2 > bestY) {
-                    result++;
-                    bestY = y2;
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+
+                if(x2 >= x1 && y1 >= y2){
+                    boolean check = false;
+                    
+                    //checking condition
+                    for(int k = 0; k < points.length; k++){
+                        if(k == i || k ==j) continue;
+                        int x3 = points[k][0];
+                        int y3 = points[k][1];
+
+                        if(x3 >= x1 && x3 <= x2 && y3 >= y2 && y3 <= y1) {
+                            check =true;
+
+                            break;
+                        }
+                    }
+                    if(!check){
+                        count++;
+                    }
                 }
             }
         }
+        
 
-        return result;
+        return count;
     }
 }
