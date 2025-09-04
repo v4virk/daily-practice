b3b8d1272c79a42551ab4744c165f2196d154b7d
HARVIJAY SINGH <harvijay.virk123@gmail.com>
2025-09-05 03:11:18 +0530

    day 27

---
+++ leetcode/237.java
@@ -0,0 +1,29 @@
+//remove an element form a linked list 
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        ListNode prev = null;
+
+        while (node != null && node.next != null) {
+            node.val = node.next.val;
+            prev = node;
+            node = node.next;
+        }
+        
+        if (node != null) {
+            if (prev != null)
+                prev.next = null;
+            else
+                node = null;
+        }
+    }
+}
