e8f6df0ef6443adb6551bbef295abab5e21cc23f
HARVIJAY SINGH <harvijay.virk123@gmail.com>
2025-09-06 13:26:06 +0530

    day 28

---
+++ leetcode/146.java
@@ -0,0 +1,24 @@
+// lru cache simplified with java.util.LinkedHashMap
+
+class LRUCache extends LinkedHashMap<Integer, Integer> {
+    private final int capacity;
+
+    public LRUCache(int capacity) {
+        // true → access-order (recently used at end)
+        super(capacity, 0.75f, true);
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
+    }
+}
